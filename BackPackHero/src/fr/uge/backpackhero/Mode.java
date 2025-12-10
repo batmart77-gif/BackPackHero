b8be710722a90ae99756c282e158c7a2d3150ef7
LassanaKonate <konatelassana282@gmail.com>
2025-12-10 19:08:26 +0100

    j'ai remplacer l'enum mode par la bonne version

--- BackPackHero/src/fr/uge/backpackhero/Mode.java
+++ BackPackHero/src/fr/uge/backpackhero/Mode.java
@@ -1,6 +1,27 @@
 package fr.uge.backpackhero;
 
+/**
+ * Represents the different possible states (modes) of the game.
+ * This enum is used to switch between exploration, combat, or end-game screens.
+ */
 public enum Mode {
-  // Les 4 états possibles du jeu
-  EXPLORATION, COMBAT, PERDU, GAGNE 
+  /**
+   * The player is exploring the dungeon map (moving between rooms).
+   */
+  EXPLORATION, 
+
+  /**
+   * The player is currently fighting enemies.
+   */
+  COMBAT, 
+
+  /**
+   * The game is over, the player has lost (0 HP).
+   */
+  PERDU, 
+
+  /**
+   * The game is won, the player has finished the dungeon.
+   */
+  GAGNE 
 }
